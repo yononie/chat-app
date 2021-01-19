a3f879a8c1ea64abbbd44ab07cb6c7d92fd0c94f
yononie <75407017+yononie@users.noreply.github.com>
2021-01-19 23:26:53 +0900

    Revert "メッセージ送信機能の実装"
    This reverts commit 3ba1e0119a0bb70ba03243b27762bcbb76277ca8.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :rooms, only: [:new, :create]
 end
